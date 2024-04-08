bd8e539bfe2a725edaa90880bcf0fbdd61b6a5b4
Rafał Rothenberger <rafal.rothenberger@gmail.com>
2024-04-08 22:59:10 +0200

    [auto] syncing settings

--- root/.config/nvim/lua/night/config.lua
+++ root/.config/nvim/lua/night/config.lua
@@ -36,3 +36,5 @@ vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 vim.keymap.set("n", "<C-[>", ":tabprevious<CR>")
 vim.keymap.set("n", "<C-]>", ":tabnext<CR>")
 
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
