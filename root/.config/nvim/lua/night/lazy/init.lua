70786109cf7697bf0ca1f37adc5d09226e0a175c
Rafał Rothenberger <rafal.rothenberger@gmail.com>
2024-07-10 19:30:50 +0200

    [auto] syncing settings

--- root/.config/nvim/lua/night/lazy/init.lua
+++ root/.config/nvim/lua/night/lazy/init.lua
@@ -2,4 +2,6 @@ return {
   "folke/which-key.nvim",
   "tpope/vim-fugitive",
   "folke/neoconf.nvim",
+  "slim-template/vim-slim",
+  "kchmck/vim-coffee-script",
 }
--- root/.config/nvim/lua/night/lazy/telescope.lua
+++ root/.config/nvim/lua/night/lazy/telescope.lua
@@ -6,7 +6,7 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
     "BurntSushi/ripgrep",
-    "rafalrothenberger/telescope-harpoon2.nvim",
+    "rrothenberger/telescope-harpoon2.nvim",
   },
 
   config = function()
