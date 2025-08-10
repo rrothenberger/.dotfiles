23d8c32aa6bbef9cae94bbe108512011be5bd406
Roksana Rothenberger <rafal.rothenberger@gmail.com>
2025-08-10 03:31:00 +0200

    [auto] syncing settings

--- extras/update_mc.sh
+++ extras/update_mc.sh
@@ -7,7 +7,8 @@ if [ -f "/opt/mc" ]; then
   echo "Removing old mc installation..."
   sudo rm /opt/mc
 fi
-sudo curl -Lo "/opt/mc" https://dl.min.io/client/mc/release/linux-amd64/mc
+# Installing specific version because in newer ones they removed admin capabilities from web UI.
+sudo curl -Lo "/opt/mc" https://dl.min.io/client/mc/release/linux-amd64/archive/mc.RELEASE.2025-04-16T18-13-26Z
 sudo chmod +x /opt/mc
 if [ -L "/usr/local/bin/mc" ] || [ -f "/usr/local/bin/mc" ]; then
   sudo rm -rf /usr/local/bin/mc
--- extras/update_minio.sh
+++ extras/update_minio.sh
@@ -7,7 +7,8 @@ if [ -f "/opt/minio" ]; then
   echo "Removing old minio installation..."
   sudo rm /opt/minio
 fi
-sudo curl -Lo "/opt/minio" https://dl.min.io/server/minio/release/linux-amd64/minio
+# Installing specific version because in newer ones they removed admin capabilities from web UI.
+sudo curl -Lo "/opt/minio" https://dl.min.io/server/minio/release/linux-amd64/archive/minio.RELEASE.2025-04-22T22-12-26Z
 sudo chmod +x /opt/minio
 if [ -L "/usr/local/bin/minio" ] || [ -f "/usr/local/bin/minio" ]; then
   sudo rm -rf /usr/local/bin/minio
