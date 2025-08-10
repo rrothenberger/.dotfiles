59db345c71018930dd14691f2152b8ef97f44466
Roksana Rothenberger <rafal.rothenberger@gmail.com>
2025-08-10 22:38:49 +0200

    [auto] syncing settings

--- root/.config/nvim/lua/night/lazy/fidget.lua
+++ root/.config/nvim/lua/night/lazy/fidget.lua
@@ -1,5 +1,15 @@
 return {
   "j-hui/fidget.nvim",
   tag = "v1.6.1",
-  opts = {},
+  opts = {
+    notification = {
+      window = {
+        zindex = 1000,
+        border = "rounded",
+        winblend = 0,
+        x_padding = 2,
+        y_padding = 2,
+      },
+    },
+  },
 }
